65dc6ed4956a4dc487e431091f53b200c10a8aed
Jinwoo Je <jeff.jw.je@gmail.com>
2024-04-28 04:43:56 +0000

    first commit

---
+++ ppva-app/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>PPVA - Personal Plan vs Actual</h1>
+    <p>select date: 4/27/2024 <button>select </button>  </p>   
+      <table>
+        <tr>
+          <td>
+            <h3>test1 </h3>
+            <p>4/27/2024 </p>
+          </td>
+          <td>
+            <h3>test2</h3>
+            <p>4/27/2024</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>
+            lorem ipsum ..... longer text how this shows we wil not no just yet lorem ipsum ..... longer text how this shows we wil not no just yet
+            lorem ipsum ..... longer text how this shows we wil not no just yetlorem ipsum ..... longer text how this shows we wil not no just yet
+            lorem ipsum ..... longer text how this shows we wil not no just yet
+            </p>
+            <button>edit</button>
+          </td>
+          <td>
+            <p>
+            lorem ipsum 2 .... lorem ipsum ..... longer text how this shows we wil not no just yet lorem ipsum ..... longer text how this shows we wil not no just yet
+            lorem ipsum ..... longer text how this shows we wil not no just yet
+            </p>
+            <button>edit</button>
+            
+          </td>
+        </tr>
+      </table>
+    </>
+
+
+  );
+}
+
+export default App;
